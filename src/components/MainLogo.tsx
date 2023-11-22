034e512ce8c4f076ec7d3fc48f3c517b8040361b
lifthus <lifthus531@gmail.com>
2023-11-22 16:59:01 +0900

    misc

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,5 +1,17 @@
-import { mainLogoCss } from "../css/mainLogo";
 import froxyLogo from "/froxy.jpg";
+
+import { css } from "@emotion/react";
+
+export const mainLogoCss = css`
+  border-radius: 25%;
+  width: 20rem;
+  padding: 1.5rem;
+  will-change: filter;
+  transition: filter 300ms;
+  &:hover {
+    filter: drop-shadow(0 0 2rem #646cffaa);
+  }
+`;
 
 type MainLogoProp = {
   href?: string;
